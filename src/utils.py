14ca1f86c31b36af69f4269ec653a2e74c27f886
ge__ <g.ecrepont@gmail.com>
2023-10-22 15:42:44 +0200

    gitignore

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+
+def compute_lead_lag_transform(self, Z: np.ndarray) -> np.ndarray:
+    """
+    This method computes the lead-lag transform Z^LL_t of each market factor process Z_t in the batch Z.
+    """
+    pass
