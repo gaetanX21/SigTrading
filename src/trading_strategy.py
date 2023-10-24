a686ae311cbdcce12ddc2b1aa4d76cda8c784733
ge__ <g.ecrepont@gmail.com>
2023-10-24 12:24:39 +0200

    nice

--- src/trading_strategy.py
+++ src/trading_strategy.py
@@ -1,18 +1,20 @@
 import utils
 import numpy as np
+import torch
+import signatory
 
 
 class TradingStrategy(object):
-    def __init__(self, N: int, delta: float) -> None:
+    def __init__(self, depth: int, delta: float) -> None:
         """
         This class implements the SigTrading strategy as defined in Signature Trading: A Path-Dependent Extension of the
         Mean-Variance Framework with Exogenous Signals (Owen Futter, Blanka Horvath, and Magnus Wiese, 2023)
 
         Arguments:
-        - N (int) : truncation level for the signature
+        - depth (int) : truncation level for the signature
         - delta (float) : risk aversion parameter
         """
-        self.N = N
+        self.depth = depth
         self.delta = delta
         self.functionals = None  # functionals l_m for m = 1, ..., d
 
@@ -23,34 +25,51 @@ class TradingStrategy(object):
         """
         return self.Z_dimension + m # or self.Z_dimension + m + 1 ?
 
-    def compute_mu_sig(self, E_ZZ_LL: np.ndarray) -> np.ndarray:
+    def compute_mu_sig(self, E_ZZ_LL: torch.Tensor) -> torch.Tensor:
         """
         This method computes the mu_sig vector as defined in the paper.
+        E_ZZ_LL needs to be computed to truncated level >=self.depth+1. (see (*))
         """
-        mu_sig = np.zeros((self.d,self.Z_dimension))
-        for multi_index in np.
+        mu_i_length = signatory.signature_channels(self.Z_dimension, self.depth)
+        mu_sig = torch.zeros((self.d,mu_i_length)) # mu_sig = [mu_1_sig, ..., mu_d_sig]
+        for m in range(self.d):
+            shift = self.f(m)
+            words = signatory.all_words(self.Z_dimension, self.depth)
+            # compute mu_i_sig
+            mu_i_sig = torch.zeros(mu_i_length)
+            for k in range(self.depth):
+                # get all words of length k
+                words_k = utils.get_level_k_words(k, self.Z_dimension)
+                # get (k+1)-th term of the signature since we have words of length k+1 (because of the shift operator)
+                level_k_signature = signatory.extract_signature_term(E_ZZ_LL, 2*self.Z_dimension, k+1) # (*)
+                # compute mu_i_sig for each word of length k
+                for word in words_k:
+                    mu_i_sig[word] = level_k_signature[word+shift]
+            mu_sig[m] = mu_i_sig
+        return mu_sig
 
-    def compute_sigma_sig(self, E_ZZ_LL: np.ndarray) -> np.ndarray:
+
+    def compute_sigma_sig(self, E_ZZ_LL: torch.Tensor) -> torch.Tensor:
         """
         This method computes the mu_sig matrix as defined in the paper.
         """
         pass
 
     def compute_functionals(
-        self, mu_sig: np.ndarray, sigma_sig: np.ndarray
-    ) -> np.ndarray:
+        self, mu_sig: torch.Tensor, sigma_sig: torch.Tensor,
+    ) -> torch.Tensor:
         """
         This method computes the functionals l_m for m = 1, ..., d as defined in the paper.
         """
         pass
 
-    def compute_xi(self, Z_LL: np.ndarray) -> np.ndarray:
+    def compute_xi(self, Z_LL: torch.Tensor) -> torch.Tensor:
         """
         This method computes the trading strategy xi_t as defined in the paper.
         """
         pass
 
-    def fit(self, X: np.ndarray, f: np.ndarray) -> None:
+    def fit(self, X: torch.Tensor, f: torch.Tensor) -> None:
         """
         This method fits the trading strategy to the data.
 
@@ -77,9 +96,9 @@ class TradingStrategy(object):
         self.Z_dimension = d + N + 1
 
         # 1. aggregate price and factor paths into market factor process Z_t = (t, X_t, f_t)
-        Z = np.zeros((M, T, d + N + 1))
+        Z = torch.zeros((M, T, self.Z_dimension))
         # time component t
-        Z[:, :, 0] = np.arange(T)  # time is defined with t_i = i
+        Z[:, :, 0] = torch.arange(T)  # time is defined with t_i = i
         # price component X_t
         Z[:, :, 1 : d + 1] = X
         # factor component f_t
@@ -92,7 +111,7 @@ class TradingStrategy(object):
         ZZ_LL = utils.compute_signature(Z_LL, self.N)
 
         # 4. compute the expected N-truncated signature E[ZZ^^LL_t] using the empirical mean
-        E_ZZ_LL = np.mean(ZZ_LL, axis=0)
+        E_ZZ_LL = torch.mean(ZZ_LL, axis=0)
 
         # 5. compute the mu_sig vector as defined in the paper
         mu_sig = self.compute_mu_sig(E_ZZ_LL)
@@ -106,7 +125,7 @@ class TradingStrategy(object):
         # print 'Fitting successful' in green
         print("\033[92m" + "Fitting successful" + "\033[0m")
 
-    def trade(self, X: np.ndarray, f: np.ndarray) -> np.ndarray:
+    def trade(self, X: torch.Tensor, f: torch.Tensor) -> torch.Tensor:
         """
         This method implements the trading strategy given new unseen data, after the model has been fitted.
 
@@ -146,7 +165,7 @@ class TradingStrategy(object):
 
         return xi
 
-    def compute_pnl(self, X: np.ndarray, xi: np.ndarray) -> float:
+    def compute_pnl(self, X: torch.Tensor, xi: torch.Tensor) -> float:
         """
         This method computes the PnL of the trading strategy when prices follow path X and the trading strategy is given by xi.
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,30 @@
 import numpy as np
+import torch
+import signatory
+import itertools
 
 
-def compute_lead_lag_transform(self, Z: np.ndarray) -> np.ndarray:
+
+def get_level_k_words(k: int, channels:int) -> np.ndarray:
     """
-    This method computes the lead-lag transform Z^LL_t of each market factor process Z_t in the batch Z.
+    This method returns all possible words of length k with letters in {0,...,channels-1}.
     """
-    pass
+    return np.array(list(itertools.product(range(channels), repeat=k)))
+
+
+def lead_lag_transform(batch_path: torch.Tensor) -> torch.Tensor:
+    """"
+    """
+    batch_path_doubled = batch_path.repeat_interleave(2, dim=1) # each path is doubled (with neighbors equal)
+    batch_lead = batch_path_doubled[:, 1:, :] # remove the first point of each path
+    batch_lag = batch_path_doubled[:, :-1, :] # remove the last point of each path
+
+    # concatenate lead and lag paths
+    batch_path_LL = torch.cat((batch_lead, batch_lag), dim=2)
+    return batch_path_LL
+
+def compute_signature(batch_path: torch.Tensor, depth: int) -> torch.Tensor:
+    """
+    """
+    signature = signatory.signature(batch_path, depth)
+    return signature
